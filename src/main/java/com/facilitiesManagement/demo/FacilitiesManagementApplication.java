be5b2b0726c644a5ff50a1b41449cf1245efb4cc
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-08-17 23:37:26 +0800

    initial commit first setup

---
+++ src/main/java/com/facilitiesManagement/demo/FacilitiesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.facilitiesManagement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitiesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitiesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facilitiesManagement/demo/FacilitiesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facilitiesManagement.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitiesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
