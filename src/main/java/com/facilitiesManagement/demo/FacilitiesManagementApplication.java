8d4fa05edc5abcc4d1d0301371f8d088e545360b
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-08-19 01:06:25 +0800

    working env variable call

--- src/main/java/com/facilitiesManagement/demo/FacilitiesManagementApplication.java
+++ src/main/java/com/facilitiesManagement/demo/FacilitiesManagementApplication.java
@@ -1,13 +1,28 @@
 package com.facilitiesManagement.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FacilitiesManagementApplication {
+	
+	@Autowired
+    private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilitiesManagementApplication.class, args);
 	}
+	
+	@GetMapping("/test")
+	public String testPage() {
 
+		System.out.println(env.getProperty("Facilities_Management_MongoDB_Uri"));
+		
+		return "test page";
+	}
 }
