7d8241e899f1480de98f670963f4864786f9c92a
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-08-19 16:50:39 +0800

    entities for room

---
+++ src/main/java/com/facilitiesManagement/core/domain/ClassRoom.java
@@ -0,0 +1,10 @@
+package com.facilitiesManagement.core.domain;
+
+import lombok.Data;
+
+@Data
+public class ClassRoom extends Room {
+
+	public Integer teacherDesk;
+	public Integer desks;
+}
---
+++ src/main/java/com/facilitiesManagement/core/domain/Room.java
@@ -0,0 +1,11 @@
+package com.facilitiesManagement.core.domain;
+
+import lombok.Data;
+
+@Data
+public class Room {
+	
+	public Integer id;
+	public String Name;
+	
+}
