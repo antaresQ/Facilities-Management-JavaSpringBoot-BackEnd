1d4607271c994a979b514d11d6a2f43661406ded
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-09-04 11:56:54 +0800

    added crossorigin workaround temporarily

--- src/main/java/com/facilitiesManagement/Adaptors/api/RoomController.java
+++ src/main/java/com/facilitiesManagement/Adaptors/api/RoomController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import com.facilitiesManagement.core.domain.RoomType;
 
 
 
-
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1/rooms")
 public class RoomController {
